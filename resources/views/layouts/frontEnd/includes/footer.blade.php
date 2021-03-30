85a4756e4e481c741d77c884e5ca472e96464f92
Emamul Murshalin <emamul@gain.media>
2021-03-30 22:51:08 +0600

    Footer currect

--- resources/views/layouts/frontEnd/includes/footer.blade.php
+++ resources/views/layouts/frontEnd/includes/footer.blade.php
@@ -3,17 +3,17 @@
         <div class="container">
             <div class="mkls_footer_grid">
                 <div class="col-xs-4 mkls_footer_grid_left">
-                    <h4>Location:</h4>
-                    <p>Dhanmondi 32,
+                    <h4 style="color: white;">Location:</h4>
+                    <p style="color: white;">Dhanmondi 32,
                         <br> Dhaka, Bangladesh</p>
                 </div>
                 <div class="col-xs-4 mkls_footer_grid_left">
                     <h4>Mail Us:</h4>
-                    <p>
+                    <p style="color: white;">
                         <span>Phone : </span>01772-222111</p>
                     <p>
-                        <span>Email : </span>
-                        <a href="mailto:info@example.com">admin@demo.com</a>
+                        <span style="color: white;">Email : </span>
+                        <a style="color: white;" href="/">admin@demo.com</a>
                     </p>
                 </div>
                 <div class="clearfix"> </div>
@@ -23,9 +23,11 @@
                     <li class="active">
                         <a href="/" class="effect-3">Home</a>
                     </li>
-                    <li>
-                        <a href="/exam/model-test" class="effect-3">Model test</a>
-                    </li>
+                    @if(auth()->user())
+                        <li>
+                            <a href="/exam/model-test" class="effect-3">Model test</a>
+                        </li>
+                    @endif
                     <li>
                         <a href="/exam/question" class="effect-3">Question</a>
                     </li>
@@ -38,9 +40,18 @@
                     <li>
                         <a href="/exam/blog" class="effect-3">Blog</a>
                     </li>
-                    <li>
-                        <a href="/user/registration" class="effect-3">Sign up</a>
-                    </li>
+                    @if(!auth()->user())
+                        <li>
+                            <a href="/user/registration" class="effect-3">Sign up</a>
+                        </li>
+                        <li>
+                            <a href="/user/login" class="effect-3">Login</a>
+                        </li>
+                    @else
+                        <li>
+                            <a href="/dashboard" class="effect-3">Dashboard</a>
+                        </li>
+                    @endif
                 </ul>
             </div>
         </div>
@@ -49,7 +60,7 @@
         <div class="container">
             <div class="allah-copy">
                 <p>© 2020 Online Preparation. All rights reserved | Design by
-                    <a href="">Tasnim Jahan</a>
+                    <a href="/">Group members</a>
                 </p>
             </div>
             <div class="footercopy-social">
